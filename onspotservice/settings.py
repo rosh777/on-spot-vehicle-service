2b90cf74bc80014d40cb0a05d069f272ef90aa31
rosh777 <roshankadu777@gmail.com>
2018-07-12 17:38:28 +0530

    Heroku setup file modified

--- onspotservice/settings.py
+++ onspotservice/settings.py
@@ -128,5 +128,3 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATIC_URL = '/static/'
-# Activate Django-Heroku.
-django_heroku.settings(locals())
