42b455e064fc8150ee5e7d3531a12fd1eff2cb22
rosh777 <roshankadu777@gmail.com>
2018-07-24 18:15:07 +0530

    Some minor changes

--- userside/models.py
+++ userside/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Vehicle(models.Model):
     brand = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
-
     fuel_choices = (('petrol', 'Petrol'),('diesel', 'Diesel'))
     fuel = models.CharField(max_length=6, choices=fuel_choices, default='diesel',)
 
