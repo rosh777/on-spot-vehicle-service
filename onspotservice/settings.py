8b96ced9d4bd3edef6365e9095b08ffc19ae5e1c
rosh777 <roshankadu777@gmail.com>
2018-07-26 18:59:03 +0530

    Change database from sqlite3 to PostgreSQL

--- onspotservice/settings.py
+++ onspotservice/settings.py
@@ -123,8 +123,12 @@ WSGI_APPLICATION = 'onspotservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'onspotservice',
+        'USER': 'onspotservice',
+        'PASSWORD': 'onspotservice',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
