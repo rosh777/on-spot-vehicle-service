957dcf39d9c383920035572f14a63c92c0fecb3a
rosh777 <roshankadu777@gmail.com>
2018-07-30 19:51:16 +0530

    Remove all dropdowns of vehicle model

--- userside/models.py
+++ userside/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 class Vehicle(models.Model):
-    vehicleType_choices = (('bike', 'Bike'), ('car', 'Car'), ('truck', 'Truck'), ('bus', 'Bus'), ('autorickshaw', 'Autorickshaw'))
-    vehicleType = models.CharField(max_length=25, choices=vehicleType_choices, default='car')
-    brand = models.CharField(max_length=255)
-    model = models.CharField(max_length=255)
-    fuel_choices = (('petrol', 'Petrol'),('diesel', 'Diesel'))
-    fuel = models.CharField(max_length=6, choices=fuel_choices, default='diesel',)
+    vehicleType = models.CharField(max_length=25)
+    brand = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    fuel = models.CharField(max_length=25)
 
     def __str__(self):
         return self.brand
